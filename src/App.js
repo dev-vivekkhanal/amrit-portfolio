4ae57095a1aa58ff5274e47327e78e734b8b141d
Vivek <dev.vivekkhanal@gmail.com>
2022-09-29 01:11:46 +0530

    partially completed contact form

--- src/App.js
+++ src/App.js
@@ -4,14 +4,45 @@ import ExperienceSection from "./components/experience/ExperienceSection";
 import HeroSection from "./components/hero/HeroSection";
 import SkillsSection from "./components/skills/SkillsSection";
 
+import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
+import ErrorOutlineRoundedIcon from "@mui/icons-material/ErrorOutlineRounded";
+import { useState } from "react";
+
 function App() {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
   return (
-    <div>
+    <div className="relative">
       <HeroSection />
       <AboutMeSection />
       <SkillsSection />
       <ExperienceSection />
-      <ContactSection />
+      <ContactSection
+        success={success}
+        setSuccess={setSuccess}
+        error={error}
+        setError={setError}
+      />
+      {/* Snackbar */}
+      <div className="fixed bottom-5 right-5 z-50">
+        {success && (
+          <div className="text-[#00ac69] bg-[#f6f4f4] p-5 rounded-xl flex justify-center items-center gap-5 transition-all">
+            <span>
+              <CheckCircleOutlineRoundedIcon />
+            </span>
+            <span>Message sent sucessfully</span>
+          </div>
+        )}
+
+        {error && (
+          <div className="text-[#ac0000] bg-[#f6f4f4] p-5 rounded-xl flex justify-center items-center gap-5 transition-all">
+            <span>
+              <ErrorOutlineRoundedIcon />
+            </span>
+            <span>Something went wrong, please try again.</span>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/contact/ContactSection.jsx
+++ src/components/contact/ContactSection.jsx
@@ -1,27 +1,45 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 
-const ContactSection = () => {
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import AlternateEmailRoundedIcon from "@mui/icons-material/AlternateEmailRounded";
+
+const ContactSection = (props) => {
   const formRef = useRef();
 
   const submitHandler = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_i98uhfi", //service ID
-        "template_ngmv1yx", //template ID
-        formRef.current,
-        "i-gSOvJ1W1yFXTqfC" //public key
-      )
-      .then(
-        (result) => {
-          console.log(result);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+    if (
+      e?.target[0]?.value?.length > 0 &&
+      e?.target[1]?.value?.length > 0 &&
+      e?.target[2]?.value?.length > 0
+    ) {
+      emailjs
+        .sendForm(
+          "service_i98uhfi", //service ID
+          "template_ngmv1yx", //template ID
+          formRef.current,
+          "i-gSOvJ1W1yFXTqfC" //public key
+        )
+        .then(
+          (result) => {
+            if (result?.status === 200) {
+              props?.setSuccess(true);
+              setTimeout(() => {
+                props?.setSuccess(false);
+              }, 5000);
+            }
+            e.target.reset();
+          },
+          (error) => {
+            props?.setError(true);
+            setTimeout(() => {
+              props?.setError(false);
+            }, 5000);
+          }
+        );
+    }
   };
 
   return (
@@ -30,35 +48,44 @@ const ContactSection = () => {
         CONTACT ME
       </h1>
 
-      <div className="flex items-center w-[80%] mx-auto">
-        <form ref={formRef} className="flex-1" onSubmit={submitHandler}>
+      <div className="flex flex-col md:flex-row  w-full  md:w-[80%] mx-auto gap-5 ">
+        <form ref={formRef} className="flex-1  w-full" onSubmit={submitHandler}>
           <input
             name="senderName"
             type="text"
             placeholder="Name"
-            className="outline-none border-0 bg-gray-200 rounded-md block p-3 px-5 w-full max-w-[400px] ml-auto mb-5"
+            className="outline-none border-0 bg-gray-200 rounded-md block p-3 px-5 w-full max-w-[400px] mx-auto md:ml-auto mb-5"
           />
           <input
             name="senderEmail"
             type="email"
             placeholder="Email"
-            className="outline-none border-0 bg-gray-200 rounded-md block p-3 px-5 w-full max-w-[400px] ml-auto mb-5"
+            className="outline-none border-0 bg-gray-200 rounded-md block p-3 px-5 w-full max-w-[400px] mx-auto md:ml-auto mb-5"
           />
           <textarea
             name="senderMessage"
             cols="30"
             rows="10"
             placeholder="Message"
-            className="outline-none border-0 bg-gray-200 rounded-md block p-3 px-5 w-full max-w-[400px] ml-auto mb-5"
+            className="outline-none border-0 bg-gray-200 rounded-md block p-3 px-5 w-full max-w-[400px] mx-auto md:ml-auto mb-5"
           ></textarea>
           <button
             type="submit"
-            className="bg-white font-semibold active:scale-95 transition-all rounded-md block p-3 px-5 w-full max-w-[400px] ml-auto mb-5"
+            className="bg-white font-semibold active:scale-95 transition-all rounded-md block p-3 px-5 w-full max-w-[400px] mx-auto md:ml-auto mb-5"
           >
             Send
           </button>
         </form>
-        <div className="flex-1"></div>
+        <div className="flex-1 text-white flex md:flex-col gap-5 justify-center items-center">
+          <div className="flex gap-2 m-5 items-center">
+            <LocationOnOutlinedIcon fontSize="large" />
+            <span>Amritsar, Punjab, India</span>
+          </div>
+          <div className="flex gap-2 m-5 items-center">
+            <AlternateEmailRoundedIcon fontSize="large" />
+            <span>amrit@paji.com</span>
+          </div>
+        </div>
       </div>
     </div>
   );
